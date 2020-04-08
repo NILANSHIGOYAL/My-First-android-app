c67a6f68883e228a085b57cc498e92a2770f8ed7
Nilanshi12 <51130168+Nilanshi12@users.noreply.github.com>
2020-04-08 19:38:45 -0400

    calling everything i need in this activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -6,14 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 Button btn;
+EmployeeDBHandler mDatabase;
+List<Employee> employeeList;
+ListView lv;
+TextView tv_EmpName, tv_EmpId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = (Button)findViewById(R.id.button);
+        tv_EmpName= (TextView)findViewById(R.id.tv_EmpName);
+        tv_EmpId = (TextView)findViewById(R.id.tv_EmpId);
+        employeeList = new ArrayList<>();
+        lv = (ListView)findViewById(R.id.lv);
+
+        mDatabase = new EmployeeDBHandler(this);
+
     }
 
     public void addNewEmployee(View view) {
