41dcc24718ec4d9bd4e055e06fe49ebca93b31b8
soniya singh <soniadhunna8@gmail.com>
2020-04-08 16:53:46 -0400

    Generating setters

--- app/src/main/java/com/example/project/Employee.java
+++ app/src/main/java/com/example/project/Employee.java
@@ -89,4 +89,60 @@ public class Employee {
     public String getCarType() {
         return carType;
     }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setOccRate(int occRate) {
+        this.occRate = occRate;
+    }
+
+    public void setEmployeeType(String employeeType) {
+        this.employeeType = employeeType;
+    }
+
+    public void setCpb(int cpb) {
+        this.cpb = cpb;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public void setSidecar(String sidecar) {
+        this.sidecar = sidecar;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
 }
