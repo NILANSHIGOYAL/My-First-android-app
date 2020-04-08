e329e5486673a405b81e2728034c04b56abb7492
soniya singh <soniadhunna8@gmail.com>
2020-04-08 17:46:10 -0400

    giving pattern for output of listview

--- app/src/main/java/com/example/project/EmployeeAdapter.java
+++ app/src/main/java/com/example/project/EmployeeAdapter.java
@@ -1,10 +1,14 @@
 package com.example.project;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
@@ -24,5 +28,14 @@ public class EmployeeAdapter extends ArrayAdapter {
         this.mDatabase=mDatabase;
     }
 
-    public View getView(){}
+    public View getView(int position, @Nullable View convertView,@Nullable ViewGroup parent ){
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View v  =  inflater.inflate(layoutRes, null);
+        TextView tv_EmpName = v.findViewById(R.id.tv_EmpName);
+        TextView tv_EmpId = v.findViewById(R.id.tv_EmpId);
+        final Employee employee = Employees.get(position);
+        tv_EmpId.setText("Id: "+ employee.getEmpId());
+        tv_EmpName.setText("Name: "+employee.getFirstName()+" "+employee.getLastName());
+        return v;
+    }
 }
