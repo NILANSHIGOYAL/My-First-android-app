68778a6d9356fd7e768012b15faa14af8fd7f33e
soniya singh <soniadhunna8@gmail.com>
2020-04-08 17:56:46 -0400

    cursors to getvalues and set to the subsequent data types

--- app/src/main/java/com/example/project/EmployeeAdapter.java
+++ app/src/main/java/com/example/project/EmployeeAdapter.java
@@ -45,6 +45,25 @@ public class EmployeeAdapter extends ArrayAdapter {
         Employees.clear();
 
         if(cursor.moveToFirst()){
+            do{
+                Employees.add(new Employee(
+                   cursor.getLong(0),
+                   cursor.getString(1),
+                        cursor.getString(2),
+                        cursor.getInt(3),
+                        cursor.getDouble(4),
+                        cursor.getInt(5),
+                        cursor.getString(6),
+                        cursor.getInt(7),
+                        cursor.getString(8),
+                        cursor.getString(9),
+                        cursor.getString(10),
+                        cursor.getString(11),
+                        cursor.getString(12),
+                        cursor.getString(13)
+                ));
+            }while (cursor.moveToNext());
+            cursor.close();
 
         }
         notifyDataSetChanged();
