4a2fe393402048514bf0de01e931b95856eb784f
soniya singh <soniadhunna8@gmail.com>
2020-04-08 17:29:31 -0400

    extending class with array adapter and adding constructor with super class

--- app/src/main/java/com/example/project/EmployeeAdapter.java
+++ app/src/main/java/com/example/project/EmployeeAdapter.java
@@ -1,4 +1,12 @@
 package com.example.project;
 
-public class EmployeeAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+public class EmployeeAdapter extends ArrayAdapter {
+    public EmployeeAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
 }
