215052e137989cb837e2313d56e0649d828ed345
soniya singh <soniadhunna8@gmail.com>
2020-04-08 16:52:10 -0400

    Generating constructor

--- app/src/main/java/com/example/project/Employee.java
+++ app/src/main/java/com/example/project/Employee.java
@@ -17,4 +17,21 @@ public class Employee {
     private String Color;
     private String carType;
 
+    public Employee(long empId, String firstName, String lastName, int age, double salary, int occRate, String employeeType, int cpb, String vehicle, String model, String plate, String sidecar, String color, String carType) {
+        this.empId = empId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+        this.occRate = occRate;
+        this.employeeType = employeeType;
+        this.cpb = cpb;
+        this.vehicle = vehicle;
+        this.model = model;
+        this.plate = plate;
+        this.sidecar = sidecar;
+        Color = color;
+        this.carType = carType;
+    }
+
 }
