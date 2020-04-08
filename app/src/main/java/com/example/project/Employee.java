b2d729a4529f9c703172a729d1f54ceb1c1a6ecd
soniya singh <soniadhunna8@gmail.com>
2020-04-08 16:49:40 -0400

    initializing all the fields  needed to get and set

--- app/src/main/java/com/example/project/Employee.java
+++ app/src/main/java/com/example/project/Employee.java
@@ -1,4 +1,20 @@
 package com.example.project;
 
 public class Employee {
+
+    private long empId;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+    private int occRate;
+    private String employeeType;
+    private int cpb;
+    private String vehicle;
+    private String model;
+    private String plate;
+    private String sidecar;
+    private String Color;
+    private String carType;
+
 }
