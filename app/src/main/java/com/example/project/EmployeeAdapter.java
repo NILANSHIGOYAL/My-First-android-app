34a4623f74b4e96c849c33914f09a47223b4db1a
soniya singh <soniadhunna8@gmail.com>
2020-04-08 17:33:45 -0400

    initializing context, layout, list and using instance of my dbhandler class

--- app/src/main/java/com/example/project/EmployeeAdapter.java
+++ app/src/main/java/com/example/project/EmployeeAdapter.java
@@ -5,7 +5,16 @@ import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 
+import java.util.List;
+
 public class EmployeeAdapter extends ArrayAdapter {
+    Context mContext;
+    int layoutRes;
+    List<Employee> Employees;
+
+    EmployeeDBHandler mDatabase;
+
+
     public EmployeeAdapter(@NonNull Context context, int resource) {
         super(context, resource);
     }
