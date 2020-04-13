f415584424a0eb7001f01894e1655473234e6362
Nilanshi12 <51130168+Nilanshi12@users.noreply.github.com>
2020-04-13 18:27:34 -0400

    visibility for side car on radio button selection

--- app/src/main/java/com/example/project/Main2Activity.java
+++ app/src/main/java/com/example/project/Main2Activity.java
@@ -26,35 +26,36 @@ import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
     Spinner empTypes, color;
-    EditText e1,e2,e3,e4,e5,e6,e7,e8,e9,e10;
-    TextView t1,t2;
+    EditText e1, e2, e3, e4, e5, e6, e7, e8, e9, e10;
+    TextView t1, t2;
     RadioButton rb1, rb2;
     RadioGroup r1, r2;
     int Age = 0;
     double annualIncome = 0.0;
     int year = Calendar.getInstance().get(Calendar.YEAR);
     EmployeeDBHandler mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         empTypes = (Spinner) findViewById(R.id.emp_Type);
-        t1 = (TextView)findViewById(R.id.textView8);
-        t2 = (TextView)findViewById(R.id.textView10);
-        e1 = (EditText)findViewById(R.id.e1);
-        e2 = (EditText)findViewById(R.id.editText6);
-        e3 = (EditText)findViewById(R.id.e3);
-        e4 = (EditText)findViewById(R.id.editText4);
-        e5 = (EditText)findViewById(R.id.editText3);
-        e6 = (EditText)findViewById(R.id.editText);
-        e7 = (EditText)findViewById(R.id.editText7);
-        e8 = (EditText)findViewById(R.id.editText5);
-        e9 = (EditText)findViewById(R.id.e9);
-        e10 = (EditText)findViewById(R.id.editText2);
-        r1 = (RadioGroup)findViewById(R.id.radioGroup1);
-        r2 = (RadioGroup)findViewById(R.id.radioGroup);
-        rb1 = (RadioButton)findViewById(R.id.radioButton3);
-        rb2 = (RadioButton)findViewById(R.id.radioButton4);
+        t1 = (TextView) findViewById(R.id.textView8);
+        t2 = (TextView) findViewById(R.id.textView10);
+        e1 = (EditText) findViewById(R.id.e1);
+        e2 = (EditText) findViewById(R.id.editText6);
+        e3 = (EditText) findViewById(R.id.e3);
+        e4 = (EditText) findViewById(R.id.editText4);
+        e5 = (EditText) findViewById(R.id.editText3);
+        e6 = (EditText) findViewById(R.id.editText);
+        e7 = (EditText) findViewById(R.id.editText7);
+        e8 = (EditText) findViewById(R.id.editText5);
+        e9 = (EditText) findViewById(R.id.e9);
+        e10 = (EditText) findViewById(R.id.editText2);
+        r1 = (RadioGroup) findViewById(R.id.radioGroup1);
+        r2 = (RadioGroup) findViewById(R.id.radioGroup);
+        rb1 = (RadioButton) findViewById(R.id.radioButton3);
+        rb2 = (RadioButton) findViewById(R.id.radioButton4);
         color = (Spinner) findViewById(R.id.color);
 
 
@@ -71,21 +72,21 @@ public class Main2Activity extends AppCompatActivity {
         empTypes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (empTypes.getSelectedItem().toString().equals("Manager")){
+                if (empTypes.getSelectedItem().toString().equals("Manager")) {
                     t1.setText("#clients");
                     t1.setVisibility(View.VISIBLE);
                     e1.setVisibility(View.VISIBLE);
                 }
-                 if (empTypes.getSelectedItem().toString().equals("Tester")){
-                     t1.setText("#bugs");
-                     t1.setVisibility(View.VISIBLE);
-                     e1.setVisibility(View.VISIBLE);
-                 }
-                 if(empTypes.getSelectedItem().toString().equals("Programmer")){
-                     t1.setText("#projects");
-                     t1.setVisibility(View.VISIBLE);
-                     e1.setVisibility(View.VISIBLE);
-                 }
+                if (empTypes.getSelectedItem().toString().equals("Tester")) {
+                    t1.setText("#bugs");
+                    t1.setVisibility(View.VISIBLE);
+                    e1.setVisibility(View.VISIBLE);
+                }
+                if (empTypes.getSelectedItem().toString().equals("Programmer")) {
+                    t1.setText("#projects");
+                    t1.setVisibility(View.VISIBLE);
+                    e1.setVisibility(View.VISIBLE);
+                }
             }
 
             @Override
@@ -108,20 +109,20 @@ public class Main2Activity extends AppCompatActivity {
         colors.add("Beige");
 
         //creating adapter for spinner
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, colors);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, colors);
         color.setAdapter(adapter1);
     }
 
     public void register(View view) {
-        if(e4.getText().toString().length() ==0)
+        if (e4.getText().toString().length() == 0)
             e4.setError("First name is required!");
-        if(e3.getText().toString().length() ==0)
+        if (e3.getText().toString().length() == 0)
             e3.setError("Last name is required!");
         //validating value for birth year between 1950 to current year
         int currentYear = Integer.parseInt(e5.getText().toString());
-        if(!(currentYear >=1950 ||
+        if (!(currentYear >= 1950 ||
                 currentYear <= year) ||
-                currentYear == 0){
+                currentYear == 0) {
             e5.setError("Birth Year is required and should be between 1950 to current year!!!");
         }
         //converting birth year to age
@@ -129,18 +130,18 @@ public class Main2Activity extends AppCompatActivity {
 
         //calculating annual income from monthly salary
         double monthlySalary = Double.parseDouble(e6.getText().toString());
-        if(monthlySalary == 0)
+        if (monthlySalary == 0)
             e6.setError("Employee Salary is required!");
-        if(e7.getText().toString().length()==0)
+        if (e7.getText().toString().length() == 0)
             e7.setError("Occupation Rate is required!");
-        if(empTypes.getSelectedItem() == null || color.getSelectedItem() == null
-        || r1.getCheckedRadioButtonId() == -1 || r2.getCheckedRadioButtonId() == -1){
+        if (empTypes.getSelectedItem() == null || color.getSelectedItem() == null
+                || r1.getCheckedRadioButtonId() == -1 || r2.getCheckedRadioButtonId() == -1) {
             Toast.makeText(this, "All Fields Required.",
                     Toast.LENGTH_SHORT).show();
         }
-        if(e9.getText().toString().length() ==0)
+        if (e9.getText().toString().length() == 0)
             e9.setError("Vehicle Model is required!");
-        if(e10.getText().toString().length() ==0)
+        if (e10.getText().toString().length() == 0)
             e10.setError("Plate Number is required!");
 
         //creating condition to add a new employee
@@ -153,7 +154,7 @@ public class Main2Activity extends AppCompatActivity {
         int cpb = Integer.parseInt(e1.getText().toString());
         String vehicle = "";
 
-        switch (r2.getCheckedRadioButtonId()){
+        switch (r2.getCheckedRadioButtonId()) {
             case R.id.radioButton3:
                 vehicle = "car";
                 break;
@@ -167,7 +168,7 @@ public class Main2Activity extends AppCompatActivity {
         String plate = e10.getText().toString();
         String Color = color.getSelectedItem().toString();
         String sidecar = "";
-        switch (r1.getCheckedRadioButtonId()){
+        switch (r1.getCheckedRadioButtonId()) {
             case R.id.radioButton5:
                 sidecar = "Yes";
                 break;
@@ -179,22 +180,39 @@ public class Main2Activity extends AppCompatActivity {
         }
 
 
-        if(mDatabase.addEmployee(firstName,lastName,age,salary,occRate,employeeType,cpb,
-                vehicle,model,plate,Color,sidecar)){
-            Toast.makeText(this,"Employee added" , Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this,MainActivity.class);
+        if (mDatabase.addEmployee(firstName, lastName, age, salary, occRate, employeeType, cpb,
+                vehicle, model, plate, Color, sidecar)) {
+            Toast.makeText(this, "Employee added", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
-        }
-        else
-            Toast.makeText(this,"Employee not added",Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(this, "Employee not added", Toast.LENGTH_SHORT).show();
 
 
     }
 
     public void onCheckedRB3(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.radioButton3:
+                if (checked) {
+                    t2.setText("Car Type:");
+                    t2.setVisibility(View.VISIBLE);
+                    e2.setVisibility(View.VISIBLE);
+                }
+        }
 
     }
 
     public void onCheckedRB4(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.radioButton4:
+                if (checked) {
+                    t2.setText("Side car:");
+                    t2.setVisibility(View.VISIBLE);
+                    r1.setVisibility(View.VISIBLE);
+                }
+        }
     }
 }
