73fc2a4c42308f9ca5bf13e29898cd7a225ab4c1
jimmy <amardeepsshetra@gmail.com>
2020-04-18 13:41:13 -0700

    getting all the data from second activity through getIntentExtra

--- app/src/main/java/com/example/project/Main3Activity.java
+++ app/src/main/java/com/example/project/Main3Activity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
     TextView tv_EN,tv_EN1,tv_EN2,tv_EN3,tv_EN4,tv_EN5,tv_EN6,tv_EN7,tv_EN8,tv_EN9,tv_EN10,tv_EN11;
+    EmployeeDBHandler db = new EmployeeDBHandler(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +25,20 @@ public class Main3Activity extends AppCompatActivity {
         tv_EN9 = (TextView) findViewById(R.id.tv_EN9);
         tv_EN10 = (TextView) findViewById(R.id.tv_EN10);
         tv_EN11= (TextView) findViewById(R.id.tv_EN11);
+
+        String empName = getIntent().getStringExtra("data");
+        String emp1 =getIntent().getStringExtra("data1");
+        int emp2 =getIntent().getIntExtra("data2",0);
+        double emp3 =getIntent().getDoubleExtra("data3",0);
+        int emp4 =getIntent().getIntExtra("data4",0);
+        String emp5 =getIntent().getStringExtra("data5");
+        String emp7 =getIntent().getStringExtra("data7");
+        String emp8 =getIntent().getStringExtra("data8");
+        String emp9 =getIntent().getStringExtra("data9");
+        String emp10 =getIntent().getStringExtra("data10");
+        String emp11=getIntent().getStringExtra("data11");
+        int emp6 =getIntent().getIntExtra("data6",0);
+        int employeeId = getIntent().getIntExtra("employee", 0);
+
     }
 }
